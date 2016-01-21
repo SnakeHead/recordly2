2906966fd88ec8118e0c4b85c5fe784f2ed96838
Gerald Bailey <gbailey@fieldsolutions.com>
2016-01-20 20:15:27 -0600

    - Created the artist submission pages.
    - Added the following packages to help with form validation:
      - check
      - audit-argument-checks
    
    - Added ability to create artists.
    - Added ability to delete artists.

--- client/templates/artists/artist.js
+++ client/templates/artists/artist.js
@@ -1,1 +1,12 @@
 // This left empty for now on purpose.
+Template.artistDetail.events({
+  'click .delete': function(e) {
+      e.preventDefault();
+
+      if (confirm("Delete this artist?")) {
+        var currentArtistId = this._id;
+        Artists.remove(currentArtistId);
+      }
+    }
+
+});
---
+++ client/templates/artists/artistAdd.js
@@ -0,0 +1,24 @@
+Template.artistSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    // Get the artist info from the form and insert it into the database (Mongo collection).
+    var artistInfo = {
+      name: $(e.target).find('[name=artistName]').val(),
+      genre: $(e.target).find('[name=artistGenre]').val()
+    };
+
+    Meteor.call('artistInsert', artistInfo, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+
+        // show this result but route anyway
+      if (result.artistExists)
+          alert('This artist has already been added.');
+
+        Router.go('artistList');
+
+    });
+  }
+});
